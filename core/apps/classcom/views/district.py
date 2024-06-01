74a2f5916031bd854974e10379d4e126ac7ae276
Samandar Azamov <jscorptech@gmail.com>
2024-06-01 12:36:54 +0000

    Update district.py

--- core/apps/classcom/views/district.py
+++ core/apps/classcom/views/district.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from core.apps.classcom.serializers import DistrictSerializer
 from core.http.choices import District
 
 
 class DistrictViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
