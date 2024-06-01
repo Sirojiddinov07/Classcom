66c826a32db0d32ecb2df82af530331f56eaddf8
Samandar Azamov <jscorptech@gmail.com>
2024-06-01 11:52:14 +0000

    🔨 Region api fixed

--- core/apps/classcom/views/region.py
+++ core/apps/classcom/views/region.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from core.apps.classcom.serializers import RegionSerializer
 from core.http.choices import Region
@@ -6,4 +6,5 @@ from core.http.choices import Region
 
 class RegionViewSet(viewsets.ModelViewSet):
     queryset = Region.objects.all()
+    permission_classes = [permissions.AllowAny]
     serializer_class = RegionSerializer
