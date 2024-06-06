4807450e8b9ea489706c04723457a2b1f307ec7f
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-06-06 14:57:59 +0500

    Tematik Plan edit

--- core/apps/classcom/models/plan.py
+++ core/apps/classcom/models/plan.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as __
 
+from core.http.models import User
+
 
 class Plan(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    banner = models.ImageField(upload_to="plan/banner/", blank=True, null=True)
+    type = models.ForeignKey("ResourceType", on_delete=models.CASCADE, null=True, blank=True)
+    hour = models.IntegerField(default=0, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     classes = models.ForeignKey("Classes", on_delete=models.CASCADE)
+    quarter = models.ForeignKey("Quarter", on_delete=models.CASCADE)
+    science = models.ForeignKey("Science", on_delete=models.CASCADE)
     topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
-    hour = models.IntegerField(default=0)
+    plan_resource = models.ForeignKey("Media", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.topic.id
