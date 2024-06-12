68cc54a275f196146106fe2bc186051fde8c32c5
Azamov Samandar <JscorpTech@gmail.com>
2024-06-12 16:15:20 +0500

    Fixed cache

--- core/apps/classcom/views/resource.py
+++ core/apps/classcom/views/resource.py
@@ -4,10 +4,16 @@ from core.apps.classcom import choices
 from core.apps.classcom import serializers
 from core.apps.classcom import permissions
 from core.http import permissions as http_permissions
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
 
 
 class ResourceViewSet(viewsets.ModelViewSet):
     queryset = models.Resource.objects.all()
+
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = self.queryset
@@ -40,5 +46,6 @@ class ResourceViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action in ["create", "update", "partial_update"]:
             return serializers.ResourceCreateSerializer
-        return serializers.ResourceDetailSerializer
-        # return serializers.ResourceSerializer
+        elif self.action == "retrieve":
+            return serializers.ResourceDetailSerializer
+        return serializers.ResourceSerializer
