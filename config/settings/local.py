2cc812dd2070645c5bd6dfa5f53237521636a8c6
Azamov Samandar <JscorpTech@gmail.com>
2024-06-19 15:33:06 +0500

    Cache fix

--- config/settings/local.py
+++ config/settings/local.py
@@ -18,29 +18,11 @@ DATABASES = {
 }
 
 MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "core.middlewares.ExceptionMiddleware",
 ]
 
-# Debug toolbar middleware
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-]
 
-INTERNAL_IPS = ("127.0.0.1",)
-
-INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
+INSTALLED_APPS += ["django_extensions"]
 
 # Allowed Hosts
 ALLOWED_HOSTS += ["127.0.0.1", "192.168.100.26"]
