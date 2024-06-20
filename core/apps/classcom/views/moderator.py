f487f0f006c41f717567555a1a0a8a4a3603ed5c
A'zamov Samandar <JscorpTech@gmail.com>
2024-06-20 15:07:21 +0500

    Reformat

--- core/apps/classcom/views/moderator.py
+++ core/apps/classcom/views/moderator.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, views, status, viewsets
+from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
