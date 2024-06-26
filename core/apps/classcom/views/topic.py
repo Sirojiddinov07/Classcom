e82cb914990987e63c9abf8e0220b42a67e0f554
SardorDevs <sardorsirojid@gmail.com>
2024-06-26 16:15:37 +0500

    filter added to topic

--- core/apps/classcom/views/topic.py
+++ core/apps/classcom/views/topic.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
 
 from core.apps.classcom import models, serializers
@@ -6,3 +7,5 @@ from core.apps.classcom import models, serializers
 class TopicViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Topic.objects.all()
     serializer_class = serializers.TopicSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['_class', 'quarter', 'science']
