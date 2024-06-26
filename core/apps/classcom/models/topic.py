4e3156621fac9d1b30215d4ddbc328ac76525fca
SardorDevs <sardorsirojid@gmail.com>
2024-06-26 12:42:39 +0500

    topic maneged

--- core/apps/classcom/models/topic.py
+++ core/apps/classcom/models/topic.py
@@ -6,6 +6,8 @@ class Topic(models.Model):
     name = models.CharField(max_length=255)
     quarter = models.ForeignKey("Quarter", on_delete=models.CASCADE)
     science = models.ForeignKey("Science", on_delete=models.CASCADE)
+    thematic_plan = models.ForeignKey("Plan", on_delete=models.CASCADE, null=True, blank=True, related_name="topics")  # Added related_name
+
 
     def __str__(self):
         return self.name
