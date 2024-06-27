85cb03cd9153fe5e703cb72e8bde20c03c3a1e9d
A'zamov Samandar <JscorpTech@gmail.com>
2024-06-27 11:21:02 +0500

    Changes to be committed: 	modified:   core/apps/classcom/apps.py 	modified:   core/apps/classcom/signals/signals.py 	modified:   core/apps/classcom/views/topic.py

--- core/apps/classcom/serializers/topic.py
+++ core/apps/classcom/serializers/topic.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from core.apps.classcom import models, serializers as core_serializers
+from .classes import ClassMiniSerializer
+from .quarter import QuarterMiniSerializer
+from .science import ScienceMiniSerializer
 
 
 class TopicSerializer(serializers.ModelSerializer):
+    _class = ClassMiniSerializer(read_only=True)
+    quarter = QuarterMiniSerializer(read_only=True)
+    _science = ScienceMiniSerializer(read_only=True)
+
     class Meta:
         model = models.Topic
         fields = "__all__"
--- core/apps/classcom/signals/signals.py
+++ core/apps/classcom/signals/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from ..models.feedback import Feedback, Answer
+from ..models.feedback import Answer
 from ..models.notification import Notification
 
 
