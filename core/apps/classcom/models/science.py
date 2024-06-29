ca572b258f2786c06f02729ab3b657936c7cdf3f
SardorDevs <sardorsirojid@gmail.com>
2024-06-29 10:35:22 +0500

    some migrations deleted

--- core/apps/classcom/models/science.py
+++ core/apps/classcom/models/science.py
@@ -4,6 +4,9 @@ from django.utils.translation import gettext_lazy as __
 
 class Science(models.Model):
     name = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
