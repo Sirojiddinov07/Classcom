9bcaed08dbbebce6dc4de4eccaf5d8a472c9e8c4
SardorDevs <sardorsirojid@gmail.com>
2024-07-11 11:43:37 +0500

    paginations added some models

--- core/apps/classcom/views/district.py
+++ core/apps/classcom/views/district.py
@@ -1,6 +1,7 @@
 from rest_framework import mixins, permissions, viewsets
 
 from core.apps.classcom.serializers import RegionDetailSerializer
+from core.apps.classcom.views.region import CustomPagination
 from core.http import models
 
 
@@ -8,3 +9,4 @@ class DistrictViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = models.Region.objects.all()
     serializer_class = RegionDetailSerializer
+    pagination_class = CustomPagination
--- core/apps/classcom/views/region.py
+++ core/apps/classcom/views/region.py
@@ -1,11 +1,18 @@
 from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from core.apps.classcom.serializers import RegionSerializer
 from core.http import models
 
+
+class CustomPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class RegionViewSet(viewsets.ModelViewSet):
     queryset = models.Region.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = RegionSerializer
     http_method_names = ["get"]
+    pagination_class = CustomPagination
