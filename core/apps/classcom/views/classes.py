2840a08a5c4da184a50b3000538821f6cbba3a23
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-07-20 14:56:44 +0500

    fix: AllowAny permissions add for science and classes

--- core/apps/classcom/views/classes.py
+++ core/apps/classcom/views/classes.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from core.apps.classcom import models, serializers
 
@@ -8,5 +9,6 @@ class ClassesViewSet(viewsets.ReadOnlyModelViewSet):
     API endpoint that allows classes to be viewed or edited.
     """
 
+    permission_classes = [AllowAny]
     queryset = models.Classes.objects.all()
     serializer_class = serializers.ClassesSerializer
