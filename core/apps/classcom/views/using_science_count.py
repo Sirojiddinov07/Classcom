f35321eea3cd20ef0682c2493cea7bffe46a199a
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-07-20 16:24:25 +0500

    fix : using science count fix

--- core/apps/classcom/views/using_science_count.py
+++ core/apps/classcom/views/using_science_count.py
@@ -1,33 +1,41 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core.apps.classcom.models.moderator import Moderator
+from core.apps.classcom.models.classes import Classes
+from core.apps.classcom.models.moderator import (
+    Moderator,
+)
 
 
 class ModeratorCountsByScienceAndClassAPIView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, science_id):
-        results = (
-            Moderator.objects.filter(science_id=science_id)
-            .values(
-                "science__id", "science__name", "classes__id", "classes__name"
+        all_classes = Classes.objects.annotate(
+            moderator_count=Count(
+                "moderator", filter=Q(moderator__science_id=science_id)
             )
-            .annotate(moderator_count=Count("id"))
-            .order_by("classes")
+        ).order_by("-moderator_count")
+
+        science_name = (
+            Moderator.objects.filter(science_id=science_id)
+            .first()
+            .science.name
+            if Moderator.objects.filter(science_id=science_id).exists()
+            else None
         )
 
         formatted_results = [
             {
-                "science_id": result["science__id"],
-                "science_name": result["science__name"],
-                "class_id": result["classes__id"],
-                "class_name": result["classes__name"],
-                "moderator_count": result["moderator_count"],
+                "class_id": cls.id,
+                "class_name": cls.name,
+                "science_id": science_id,
+                "science_name": science_name,
+                "moderator_count": cls.moderator_count,
             }
-            for result in results
+            for cls in all_classes
         ]
 
         return Response(formatted_results)
