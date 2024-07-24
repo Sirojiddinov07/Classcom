e813904e87f3b0f16102b177acc3c611bb3f09d6
A'zamov Samandar <JscorpTech@gmail.com>
2024-07-24 12:43:57 +0500

    update enpoind.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 poetry run python3 manage.py migrate --no-input
 poetry run python3 manage.py collectstatic --no-input
-poetry run gunicorn config.wsgi --reload --bind 0.0.0.0:8000
+poetry run gunicorn config.wsgi --reload --bind 0.0.0.0:8000 &
+poetry run celery -A config worker -l info
 
 exit $?
