87378532e82d96d1fec0dab464ce77799bbb9e63
SardorDevs <sardorsirojid@gmail.com>
2024-07-24 14:46:50 +0500

    science group added to science model

--- core/apps/classcom/views/science.py
+++ core/apps/classcom/views/science.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from core.apps.classcom import models, serializers
@@ -6,3 +7,5 @@ from core.apps.classcom import models, serializers
 class ScienceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Science.objects.all()
     serializer_class = serializers.ScienceSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["science_grp", "name"]
