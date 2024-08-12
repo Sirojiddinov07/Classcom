fb629bc6e66f3f25a1b9b0e375eb42b98e410cad
A'zamov Samandar <JscorpTech@gmail.com>
2024-08-12 18:37:35 +0500

    Update resource fields to include subtype

--- core/apps/classcom/views/resource.py
+++ core/apps/classcom/views/resource.py
@@ -26,7 +26,7 @@ class ResourceViewSet(viewsets.ModelViewSet):
     queryset = models.Resource.objects.all()
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
-    fields = ["name", "type", "classes", "category", "category_type"]
+    fields = ["name", "type", "classes", "subtype", "category", "category_type"]
 
     @method_decorator(never_cache)
     def dispatch(self, request, *args, **kwargs):
@@ -43,41 +43,3 @@ class ResourceViewSet(viewsets.ModelViewSet):
                 return serializers.ResourceDetailSerializer
         return serializers.ResourceSerializer
 
-    def filter_queryset(self, queryset):
-        queryset = super().filter_queryset(queryset)
-
-        if "category_type" in self.request.query_params:
-            all_classes = models.Classes.objects.all()
-
-            filtered_resources = queryset
-
-            filtered_class_ids = filtered_resources.values_list(
-                "classes_id", flat=True
-            )
-            classes_not_in_filtered_resources = all_classes.exclude(
-                id__in=filtered_class_ids
-            )
-
-            self.extra_context = {
-                "resources": filtered_resources,
-                "classes_not_in_filtered_resources": classes_not_in_filtered_resources,
-            }
-        else:
-            self.extra_context = {}
-        return queryset
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-
-        classes_serializer = serializers.ClassesSerializer(
-            self.extra_context.get("classes_not_in_filtered_resources", []),
-            many=True,
-        )
-
-        response_data = {
-            "resources": serializer.data,
-            "classes_not_in_filtered_resources": classes_serializer.data,
-        }
-
-        return Response(response_data)
