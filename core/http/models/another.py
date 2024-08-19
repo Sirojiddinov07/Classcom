04dafa8be509c8ae7c64d53e14ba02ae91ec477a
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-08-19 14:59:30 +0500

    fix:tarsnlate

--- core/http/models/another.py
+++ core/http/models/another.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from polymorphic import models as polymorphic
 
 from core.http.models.base import AbstractBaseModel
