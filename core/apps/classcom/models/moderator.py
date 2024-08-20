c9b731358163de11aaf4e80d1e5b90feb8fefc30
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-08-20 14:00:28 +0500

    fix: admin panel fix

--- core/apps/classcom/models/moderator.py
+++ core/apps/classcom/models/moderator.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as __
+from django.utils.translation import gettext_lazy as _
 
 from core.apps.classcom import choices
 from core.apps.classcom.choices import Role
@@ -9,50 +9,50 @@ from core.http.models import AbstractBaseModel
 
 class Moderator(AbstractBaseModel):
     user = models.OneToOneField(
-        "http.User", on_delete=models.CASCADE, verbose_name=__("Foydalanuvchi")
+        "http.User", on_delete=models.CASCADE, verbose_name=_("Foydalanuvchi")
     )
-    balance = models.BigIntegerField(default=0, verbose_name=__("Balans"))
+    balance = models.BigIntegerField(default=0, verbose_name=_("Balans"))
     science = models.ForeignKey(
         to="Science",
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name=__("Fan"),
+        verbose_name=_("Fan"),
     )
     science_type = models.ForeignKey(
         ScienceTypes,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name=__("Fan turi"),
+        verbose_name=_("Fan turi"),
     )
     degree = models.CharField(
         max_length=15,
         choices=choices.Degree.choices,
         default=choices.Degree.AUTHOR,
-        verbose_name=__("Daraja"),
+        verbose_name=_("Daraja"),
     )
     docs = models.FileField(
         upload_to="documents/",
         null=True,
         blank=True,
-        verbose_name=__("Hujjat"),
+        verbose_name=_("Hujjat"),
     )
     is_contracted = models.BooleanField(
-        default=False, verbose_name=__("Shartnoma")
+        default=False, verbose_name=_("Shartnoma")
     )
     plan_creatable = models.BooleanField(
-        default=True, verbose_name=__("Tematik Reja yarata olishi")
+        default=True, verbose_name=_("Tematik Reja yarata olishi")
     )
     resource_creatable = models.BooleanField(
-        default=True, verbose_name=__("Resurs yarata olishi.")
+        default=True, verbose_name=_("Resurs yarata olishi.")
     )
 
     def __str__(self) -> str:
         return str(self.user.first_name)
 
     class Meta:
-        verbose_name = __("Moderator")
-        verbose_name_plural = __("Moderatorlar")
+        verbose_name = _("Moderator")
+        verbose_name_plural = _("Moderatorlar")
 
     def save(self, *args, **kwargs):
         if self.user.role != Role.MODERATOR:
@@ -75,34 +75,34 @@ class Moderator(AbstractBaseModel):
 
 class TempModerator(AbstractBaseModel):
     user = models.OneToOneField(
-        "http.User", on_delete=models.CASCADE, verbose_name=__("Foydalanuvchi")
+        "http.User", on_delete=models.CASCADE, verbose_name=_("Foydalanuvchi")
     )
-    balance = models.BigIntegerField(default=0, verbose_name=__("Balans"))
+    balance = models.BigIntegerField(default=0, verbose_name=_("Balans"))
     science = models.ForeignKey(
         to="Science",
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name=__("Fan"),
+        verbose_name=_("Fan"),
     )
     science_type = models.ForeignKey(
         ScienceTypes,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name=__("Fan turi"),
+        verbose_name=_("Fan turi"),
     )
     degree = models.CharField(
         max_length=15,
         choices=choices.Degree.choices,
         default=choices.Degree.AUTHOR,
-        verbose_name=__("Daraja"),
+        verbose_name=_("Daraja"),
     )
     docs = models.FileField(
         upload_to="documents/",
         null=True,
         blank=True,
-        verbose_name=__("Hujjat"),
+        verbose_name=_("Hujjat"),
     )
     is_contracted = models.BooleanField(
-        default=False, verbose_name=__("Shartnoma")
+        default=False, verbose_name=_("Shartnoma")
     )
