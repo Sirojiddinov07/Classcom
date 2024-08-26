bd052a64f1a61e3ff5db16182e0b5e5929582595
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-08-26 17:42:44 +0500

    fix: media post fix

--- core/apps/classcom/serializers/media.py
+++ core/apps/classcom/serializers/media.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from core.apps.classcom.models import Media
@@ -13,6 +15,10 @@ class MediaSerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
+        if validated_data.get("desc") is None:
+            validated_data["desc"] = validated_data[
+                "file"
+            ].name + datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
         media = Media.objects.create(**validated_data)
         return media
 
