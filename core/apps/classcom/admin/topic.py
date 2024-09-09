7f9b1c61917facfe01a1484b123c06138a72bb40
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-09-09 18:02:39 +0500

    plan filter add for topic

--- core/apps/classcom/admin/topic.py
+++ core/apps/classcom/admin/topic.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from unfold.admin import ModelAdmin
 
+from core.apps.classcom.filters import PlanFilter
 from core.apps.classcom.models import Topic
 from core.apps.classcom.resources import TopicResource
 
@@ -15,14 +16,19 @@ class TopicAdmin(ModelAdmin, ImportExportModelAdmin):
     """
 
     resource_class = TopicResource
+    list_filter_submit = True
     list_display = (
         "id",
         "sequence_number",
         "name",
         "description",
+        "hours",
+        "weeks",
+        "media_creatable",
     )
     search_fields = (
         "name",
         "description",
     )
     filter_horizontal = ("media",)
+    list_filter = (PlanFilter, "media_creatable")
