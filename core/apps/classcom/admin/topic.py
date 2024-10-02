c9d7e8fc3e5516fb1af972790aa9307628d95307
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-02 11:53:12 +0500

    fix: topic admin

--- core/apps/classcom/admin/topic.py
+++ core/apps/classcom/admin/topic.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from import_export.admin import ImportExportModelAdmin
 from unfold.admin import ModelAdmin
 
@@ -30,5 +31,29 @@ class TopicAdmin(ModelAdmin, ImportExportModelAdmin):
         "name",
         "description",
     )
+    list_editable = ("media_creatable",)
     filter_horizontal = ("media",)
     list_filter = (PlanFilter, "media_creatable")
+    actions = ["make_inactive", "make_active"]
+
+    def make_inactive(self, request, queryset):
+        """Make selected topics inactive
+
+        Args:
+            request (_type_): _description_
+            queryset (_type_): _description_
+        """
+        queryset.update(media_creatable=False)
+
+    make_inactive.short_description = _("Nofaol qilish")
+
+    def make_active(self, request, queryset):
+        """Make selected topics active
+
+        Args:
+            request (_type_): _description_
+            queryset (_type_): _description_
+        """
+        queryset.update(media_creatable=True)
+
+    make_active.short_description = _("Faol qilish")
