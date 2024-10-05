4ae2102fb0c1148a1c206bdafed8440876fb7746
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-05 16:19:45 +0500

    fix: search add

--- core/apps/classcom/serializers/topic.py
+++ core/apps/classcom/serializers/topic.py
@@ -14,15 +14,20 @@ class TopicSerializer(serializers.ModelSerializer):
             "hours",
             "weeks",
             "sequence_number",
+            "banner",
         ]
 
     def create(self, validated_data):
-        topic = models.Topic.objects.create(**validated_data)
+        user = self.context["request"].user
+        topic = models.Topic.objects.create(**validated_data, user=user)
         return topic
 
 
 class TopicDetailSerializer(serializers.ModelSerializer):
     media = MediaDetailSerializer(many=True)
+    user = serializers.SerializerMethodField()
+    media_count = serializers.SerializerMethodField()
+    download_count = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Topic
@@ -36,20 +41,21 @@ class TopicDetailSerializer(serializers.ModelSerializer):
             "media_creatable",
             "weeks",
             "media",
+            "banner",
+            "user",
+            "view_count",
+            "media_count",
+            "download_count",
         ]
         extra_kwargs = {"media": {"required": False}}
 
+    def get_user(self, obj):
+        from core.http.serializers.user import UserSerializer
 
-class TopicNoPaidSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Topic
-        fields = [
-            "id",
-            "name",
-            "description",
-            "hours",
-            "weeks",
-            "sequence_number",
-            "created_at",
-            "media_creatable",
-        ]
+        return UserSerializer(obj.user).data
+
+    def get_media_count(self, obj):
+        return obj.media_count
+
+    def get_download_count(self, obj):
+        return obj.all_download_count
