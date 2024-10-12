defe9e2ba43a31a605c733b4d2035ccee9076496
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-12 10:50:51 +0500

    fix: using count add

--- core/apps/classcom/serializers/science.py
+++ core/apps/classcom/serializers/science.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 
-from core.apps.classcom.models import Science, ScienceTypes
+from core.apps.classcom.models import Science, ScienceTypes, Moderator
 
 
 class ScienceTypesSerializer(serializers.ModelSerializer):
+    count = serializers.SerializerMethodField()
+
     class Meta:
         model = ScienceTypes
-        fields = ("id", "name")
+        fields = ("id", "name", "count")
+
+    def get_count(self, obj):
+        return Moderator.objects.filter(science_type=obj).count()
 
 
 class ScienceSerializer(serializers.ModelSerializer):
     types = ScienceTypesSerializer(many=True)
+    count = serializers.SerializerMethodField()
 
     class Meta:
         model = Science
@@ -21,7 +27,11 @@ class ScienceSerializer(serializers.ModelSerializer):
             "types",
             "class_group",
             "created_at",
+            "count",
         ]
+
+    def get_count(self, obj):
+        return Moderator.objects.filter(science=obj).count()
 
 
 class ScienceMiniSerializer(serializers.ModelSerializer):
