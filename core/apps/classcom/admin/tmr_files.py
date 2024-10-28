c002115918ccf21193b817b89c309874a175803c
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-10-28 13:03:07 +0500

    fix: tmr appeal add

--- core/apps/classcom/admin/tmr_files.py
+++ core/apps/classcom/admin/tmr_files.py
@@ -6,7 +6,7 @@ from core.apps.classcom.models import TmrFiles
 
 @admin.register(TmrFiles)
 class TmrFilesAdmin(ModelAdmin):
-    list_display = ("title", "description", "file", "type", "size")
+    list_display = ("id", "title", "description", "file", "type", "size")
     search_fields = ("title", "description", "file", "type")
     list_filter = ("is_active", "type")
     readonly_fields = ("size", "type")
