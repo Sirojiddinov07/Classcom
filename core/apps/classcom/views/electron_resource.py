842da0d2f604952aeb2466e895842e18f9f1559f
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-01-27 15:05:37 +0500

    fix: electron resource add

--- core/apps/classcom/views/electron_resource.py
+++ core/apps/classcom/views/electron_resource.py
@@ -153,7 +153,7 @@ class ElectronResourceView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
